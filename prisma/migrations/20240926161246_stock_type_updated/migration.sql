1c732b41a93365a7dbe2f5fb6984754b14042045
Palash Nath <palashnath880@gmail.com>
2024-09-26 22:29:44 +0600

    scrap model added and stock type updated

--- prisma/migrations/20240926161246_stock_type_updated/migration.sql
+++ prisma/migrations/20240926161246_stock_type_updated/migration.sql
@@ -1,2 +1,2 @@
 -- AlterTable
-ALTER TABLE `Stock` MODIFY `type` ENUM('entry', 'transfer', 'faulty', 'defective', 'scrap', 'fromFaulty', 'faultyScrap', 'purchaseReturn') NOT NULL DEFAULT 'entry';
+ALTER TABLE `stock` MODIFY `type` ENUM('entry', 'transfer', 'faulty', 'defective', 'scrap', 'fromFaulty', 'faultyScrap', 'purchaseReturn') NOT NULL DEFAULT 'entry';
---
+++ prisma/migrations/20240926162849_scrap_model_created_and_stock_schema_updated/migration.sql
@@ -0,0 +1,37 @@
+/*
+  Warnings:
+
+  - The values [scrap,faultyScrap] on the enum `Stock_type` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE `Stock` MODIFY `type` ENUM('entry', 'transfer', 'faulty', 'defective', 'fromFaulty', 'purchaseReturn') NOT NULL DEFAULT 'entry';
+
+-- CreateTable
+CREATE TABLE `Scrap` (
+    `id` VARCHAR(191) NOT NULL,
+    `challanNo` VARCHAR(191) NOT NULL,
+    `note` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `Scrap_challanNo_key`(`challanNo`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ScrapItem` (
+    `id` VARCHAR(191) NOT NULL,
+    `skuCodeId` VARCHAR(191) NOT NULL,
+    `quantity` DOUBLE NOT NULL,
+    `scrapId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ScrapItem` ADD CONSTRAINT `ScrapItem_skuCodeId_fkey` FOREIGN KEY (`skuCodeId`) REFERENCES `SkuCode`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ScrapItem` ADD CONSTRAINT `ScrapItem_scrapId_fkey` FOREIGN KEY (`scrapId`) REFERENCES `Scrap`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
